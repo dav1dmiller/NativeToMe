cd66e0a0a8c37a0f68fde3cd8ab02d5ada062154
ralira2062 <ralphylira@gmail.com>
2020-04-07 12:15:31 -0700

    .

--- accounts/userprofile/views.py
+++ accounts/userprofile/views.py
@@ -13,8 +13,10 @@ from django.dispatch import receiver
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email')
-
+        fields = ('first_name',
+                  'last_name',
+                  'email')
+        
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
